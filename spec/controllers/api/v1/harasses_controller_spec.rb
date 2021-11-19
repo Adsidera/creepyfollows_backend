92be3559a16f7bfeeb8774a8b2f72f75f10c38d8
Anna Costalonga <annamaria.costalonga@hpi.de>
2021-11-20 00:22:46 +0100

    created more robust specs

--- spec/controllers/api/v1/harasses_controller_spec.rb
+++ spec/controllers/api/v1/harasses_controller_spec.rb
@@ -2,17 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::HarassesController, type: :request do
   describe 'GET /api/v1/harasses' do
-    let(:url) { "/api/v1/harasses"}
+    let(:url) { '/api/v1/harasses'}
     before do
-      @harasses = [ Fabricate(:harass) ]
+      @harasses = [ Fabricate(:harass, happened_at: Date.iso8601('2021-11-19'), description: 'A man followed me') ]
       get url
     end
 
     it 'has a response with 200 status code' do
       expect(response).to have_http_status(200)
+      expect(JSON.parse(response.body)['data'].first['attributes']).to  match(
+        hash_including(
+          {
+            'start_address'=>'Ackerstr.18, Berlin',
+            'description'=>'A man followed me',
+            'longitude'=>13.3970128,
+            'latitude'=>52.5314368,
+            'when'=>'19 Nov 2021'}
+        )
+      )
     end
 
-    it "JSON body response contains expected harass attributes" do
+    it 'JSON body response contains expected harass attributes' do
       json_response = JSON.parse(response.body)
       expect(json_response).not_to be_empty
       expect(json_response).to include{@harasses.to_json}
@@ -20,9 +30,29 @@ RSpec.describe Api::V1::HarassesController, type: :request do
   end
 
   describe 'POST /api/v1/harasses' do
+    subject(:action) { post '/api/v1/harasses', params: params }
+
+    let(:params) do
+      {
+        harass:
+          {
+            start_address: 'Akazienstr.11, Berlin',
+            happened_at: Date.iso8601('2021-11-19'),
+            description: 'A creepy follow'
+          }
+      }
+    end
+
     it 'POST successfully' do
-      post '/api/v1/harasses', params: { harass: {start_address: "Akazienstr.11, Berlin", happened_at: Date.today, description: "A creepy follow"} }
+      action
+
       expect(response).to have_http_status(200)
+      expect(JSON.parse response.body).to match(
+        hash_including(
+          'status'=>'OK',
+          'message'=>'created harass case',
+        )
+      )
     end
   end
 end
