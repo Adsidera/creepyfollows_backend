359bca49e35df176fb5ea1e93faeee2b44df58e1
Anna Costalonga <annamaria.costalonga@hpi.de>
2023-04-11 19:53:23 +0200

    added defaults

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ Bundler.require(*Rails.groups)
 module CreepyFollowsBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 7.0
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
